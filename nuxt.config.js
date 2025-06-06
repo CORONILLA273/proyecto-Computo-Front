9021996272d24c2b95a3be044923edcb859571b2
Coronilla <e.coronillahernandez@ugto.com>
2025-06-06 02:06:07 -0600

    Front terminado :)

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.baseURL || 'http://localhost:5050/api'
+    baseURL: process.env.baseURL || 'https://backend-inventory-computo-a7e8cjgufghuanhb.canadacentral-01.azurewebsites.net/api'
   },
 
   auth: {
